c21475b4db90acb7a65c14ef79a1d1493be51c31
Arturo Mayta <arturomc@unjbg.edu.pe>
2023-07-03 17:51:14 -0500

    producto

---
+++ DTO/CreateProductDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace ApiRestaurant.DTO
+{
+    public class CreateProductDTO
+    {
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public IFormFile name_file { get; set; }
+        public bool available { get; set; }
+        public int categoryId { get; set; }
+    }
+}
---
+++ DTO/ProductDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace ApiRestaurant.DTO
+{
+    public class ProductDTO
+    {
+
+        public string name { get; set; }
+        public string description { get; set; }
+        public decimal price { get; set; }
+        public IFormFile name_file { get; set; }
+        public bool available { get; set; }
+        public int categoryId { get; set; }
+    }
+}
